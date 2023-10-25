888575a2d11389fe34db0376e262b14938a95f33
Arivald8 <Arivald88@gmail.com>
2023-10-25 16:22:39 +0100

    Added a __setattr__ method to the user model. This method checks if the instance type of __value has a matching type in the attr_types dictionary, and if not, raised a ValueError. Added additional tests into tester.py which cover the new __setattr__ method as well as object instantiation.

--- src/models/user.py
+++ src/models/user.py
@@ -1,36 +1,40 @@
+"""
+User Model definition for the authentication system
+
+This module defines the User class, represeting a user in the authentication system.
+It icludes user attributes, methods for authentication, authorization and user data.
+
+Attrs:
+    - Username (str): A unique username.
+    - Password (str): The securely hashed password for user authentication.
+    - Email (Optional) (str): The email address associate with the user.
+    - is_admin (bool): Indicated whether the user has admin privileges.
+    - created (datetime): The date and time when the user registered.
+
+Methods:
+    - authenticate(password): Verify the user's password for authentication. 
+    - has_permission(permission): Check if the user has a specific permission.
+    - update_profile(new_data): Update user profile information.
+    - change_password(new_password): Change the user's password securely.
+    - generate_reset_token(): Generate a token for password reset. 
+
+Author: Arivald8
+Date: 25th Oct 2023
+"""
+
+
 from datetime import datetime
+from typing import Any
+
 
 class User:
-    """
-    User Model definition for the authentication system
-
-    This module defines the User class, represeting a user in the authentication system.
-    It icludes user attributes, methods for authentication, authorization and user data.
-
-    Attrs:
-        - Username (str): A unique username.
-        - Password (str): The securely hashed password for user authentication.
-        - Email (Optional) (str): The email address associate with the user.
-        - is_admin (bool): Indicated whether the user has admin privileges.
-        - created (datetime): The date and time when the user registered.
-
-    Methods:
-        - authenticate(password): Verify the user's password for authentication. 
-        - has_permission(permission): Check if the user has a specific permission.
-        - update_profile(new_data): Update user profile information.
-        - change_password(new_password): Change the user's password securely.
-        - generate_reset_token(): Generate a token for password reset. 
-
-    Author: Arivald8
-    Date: 25th Oct 2023
-    """
     def __init__(
         self, 
-        username=None, 
-        password=None, 
-        email=None, 
-        is_admin=None,
-        created=datetime.now()
+        username: str = "", 
+        password: str = "", 
+        email: str = "", 
+        is_admin: bool = False,
+        created: datetime = datetime.now()
     ):
         self.created = created
         self.username = username
@@ -40,7 +44,27 @@ class User:
         self.created = created
 
 
-    def __str__(self):
-        return f"""Username: {self.username} \nAdmin: {self.is_admin} \nCreated: {self.created}"""
+    def __str__(self) -> str:
+        return f"Username: {self.username} \nAdmin: {self.is_admin} \nCreated: {self.created}"
 
     
+    def __setattr__(self, __name: str, __value: Any) -> None:
+        attr_types = {
+            "username": str,
+            "password": str,
+            "email": str,
+            "is_admin": bool,
+            "created": datetime
+        }
+
+        if not isinstance(__value, attr_types[__name]):
+            print(f"__value {__value} and attr {__name}")
+            raise ValueError(
+                f"""
+                Incorrect instance type {type(__value)} provided to attribute {__name}.
+                Attribute {__name} should be of type {attr_types[__name]}
+                """
+            )
+            
+        else:
+            super().__setattr__(__name, __value)
--- tester.py
+++ tester.py
@@ -16,18 +16,44 @@ class TestSrcVersion(unittest.TestCase):
 class TestUserModel(unittest.TestCase):
     def setUp(self) -> None:
         self.user = User()
+        self.user.username = "tester"
+        self.user.password = "tester"
+        self.user.email = "tester"
+        self.user.is_admin = False
+
         return super().setUp()
 
     
     def test_user_object_instantiation(self):
         self.assertIsInstance(self.user, User)
-        self.assertIsNone(self.user.username)
-        self.assertIsNone(self.user.password)
-        self.assertIsNone(self.user.email)
-        self.assertIsNone(self.user.is_admin)
+        self.assertEqual(self.user.username, "tester")
+        self.assertEqual(self.user.password, "tester")
+        self.assertEqual(self.user.email, "tester")
+        self.assertFalse(self.user.is_admin)
 
 
     def test_user_object_datetime_attribute(self):
         self.assertIsNotNone(self.user.created)
         self.assertIsInstance(self.user.created, datetime)
+
+
+    def test_user_object_attributes_setter(self):
+        self.user.__setattr__("username", "tester")
+        self.user.__setattr__("password", "tester")
+        self.user.__setattr__("email", "tester")
+        self.user.__setattr__("is_admin", True)
+
+        self.assertEqual(self.user.username, "tester")
+        self.assertEqual(self.user.password, "tester")
+        self.assertEqual(self.user.email, "tester")
+        self.assertEqual(self.user.is_admin, True)
+
     
+    def test_user_object_setter_raise_value_errors(self):
+        with self.assertRaises(ValueError):
+            self.user.__setattr__("username", 0)
+            self.user.__setattr__("password", 0)
+            self.user.__setattr__("email", 0)
+            self.user.__setattr__("is_admin", 0)
+            self.user.__setattr__("created", 0)
+        
